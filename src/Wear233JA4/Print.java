ecf05d6f22b3820d7c0e82a7e8580f15b5fd5bf3
Adam Wear <Adam Wear>
2016-10-25 09:08:32 -0700

    first commit

---
+++ src/Wear233JA4/Print.java
@@ -0,0 +1,25 @@
+package Wear233JA4;
+
+/**
+ * This class prints the message "Hello world!"
+ * @author Adam Wear
+ * @ version 10/25/2016.
+ */
+public class Print
+{
+    /**
+     * Constructor method.
+     */
+    public Print()
+    {
+        printGreeting();
+    }
+
+    /**
+     * Prints a greeting for the user.
+     */
+     private static void printGreeting()
+    {
+        System.out.println("Hello world!");
+    }
+}
